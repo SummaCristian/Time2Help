a0ad92e57aa4a0b6366161e22f05be8492a6a9c4
Cristian Summa <cristian.summa@mail.polimi.it>
2024-10-18 21:27:54 +0200

    Updated the Map
    The Map now defaults to tracking the User's direction via gyroscope and magnetometer.
    This behavior can be toggled off via the Compass indicator in the top right corner, however it's now always on by default.
    
    The Map now allows to click on any of the default Markers, allowing to better see the name and the type of Marker of the place it belongs to.

--- Views/MapView.swift
+++ Views/MapView.swift
@@ -6,7 +6,8 @@ import MapKit
 struct MapView: View {
     // The ViewModel, where the Data and Permission are handled.
     @ObservedObject var viewModel: MapViewModel
-    @State private var mapCameraPosition: MapCameraPosition = .userLocation(followsHeading: false, fallback: .automatic)
+    @State private var mapCameraPosition: MapCameraPosition = .userLocation(followsHeading: true, fallback: .automatic)
+    @State private var selection: MapFeature? = nil
     @Namespace private var mapNameSpace
     
     // The UI
@@ -14,8 +15,7 @@ struct MapView: View {
         // Bundling everything in a ZStack, so that a background blur effect can be applied behind the TabView
         ZStack {
             // The Map, centered around ViewModel's region, and showing the User's position when possible
-            //Map(coordinateRegion: $viewModel.region, interactionModes: .all, showsUserLocation: true)
-            Map(initialPosition: mapCameraPosition, bounds: MapCameraBounds(minimumDistance: 0.008, maximumDistance: .infinity), interactionModes: .all, scope: mapNameSpace) {
+            Map(initialPosition: mapCameraPosition, bounds: MapCameraBounds(minimumDistance: 0.008, maximumDistance: .infinity), interactionModes: .all, selection: $selection, scope: mapNameSpace) {
                 UserAnnotation()
             }
             .mapControlVisibility(.visible)
@@ -25,13 +25,14 @@ struct MapView: View {
                 MapScaleView()
                 MapPitchToggle()
             }
+            .mapStyle(.standard(elevation: .realistic, emphasis: .automatic, pointsOfInterest: .all, showsTraffic: false)    )
             .onAppear {
                 // When the Map appears on-screen, check permissions and update location
                 viewModel.checkIfLocationServicesIsEnabled()
             }
-            .onChange(of: viewModel.region) { oldRegion, newRegion in
+            /*.onChange(of: viewModel.region) { oldRegion, newRegion in
                 mapCameraPosition = .region(viewModel.region)
-            }
+            }*/
             .edgesIgnoringSafeArea(.bottom)
             .tint(.blue)
         }
