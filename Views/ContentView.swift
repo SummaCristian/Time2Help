5be2da1b35858104da13dbc33a4b9c4a08d63641
Cristian Summa <cristian.summa@mail.polimi.it>
2024-10-18 21:25:37 +0200

    Fixed a bug with New Favor sheet
    Fixed a bug where the New Favor sheet would immediately close itself the first time it was opened after cold-launching the app.
    
    Note: this bug seems to be still present in the Profile tab, but should now be fixed from the Map tab.
    Still working on it...

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -9,54 +9,62 @@ struct ContentView: View {
     @State private var selectedTab: Int = 0 // Track the selected tabEmptyView
     // Boolean value to hanlde the behavior of the "New Favor" sheet
     @State private var isSheetPresented = false // State to control sheet visibility
+    // Boolean value to handle the behavior of the "Export IPA" sheet
     @State private var isExportSheetPresented = false
+    
+    
     // Main View
     var body: some View {
         // The main UI of this app is composed of a TabView, meaning the the UI is divided
         // into different tabs, each one containing a portion of the app.
         // Each one of the different screens is then coded inside its own file.
-        TabView(selection: $selectedTab) {
-            Group {
-                // Tab 0: Map
-                MapView(viewModel: viewModel)
-                    .tabItem {
-                        Label("Mappa", systemImage: "map")
-                    }
-                    .tag(0) // Tag for the Map tab
-                
-                // Tab 1: New Favor (opens sheet)
-                Text("Nuovo Favore") // A placeholder just for the tab label
-                    .tabItem {
-                        Label("Nuovo Favore", systemImage: "plus.rectangle")
-                    }
-                    .tag(1) // Tag for the Nuovo Favore tab
-                    .onChange(of: selectedTab) { oldValue, newValue in
-                        if newValue == 1 {
-                            isSheetPresented = true // Show the sheet when the tab is selected
-                            selectedTab = oldValue // Return to the previous tab
+        ZStack(alignment: .bottom) {
+            TabView(selection: $selectedTab) {
+                Group {
+                    // Tab 0: Map
+                    MapView(viewModel: viewModel)
+                        .tabItem {
+                            Label("Mappa", systemImage: "map")
                         }
-                    }
-                
-                // Tab 2: Profile
-                ProfileView()
-                    .tabItem {
-                        Label("Profilo", systemImage: "person.fill")
-                    }
-                    .tag(2) // Tag for the Profile tab
-                // Temporary: need a way to export an IPA file without XCode
-                    .onLongPressGesture(minimumDuration: 0.5, maximumDistance: 10) { 
-                        isExportSheetPresented = true
-                    }
-                
+                        .tag(0) // Tag for the Map tab
+                    
+                    // Tab 1: New Favor (opens sheet)
+                    Text("Nuovo Favore") // A placeholder just for the tab label
+                     .tabItem {
+                         Label("Nuovo Favore", systemImage: "plus.rectangle")
+                     }
+                     .tag(1) // Tag for the Nuovo Favore tab
+                    
+                    // Tab 2: Profile
+                    ProfileView()
+                        .tabItem {
+                            Label("Profilo", systemImage: "person.fill")
+                        }
+                        .tag(2) // Tag for the Profile tab
+                    // Temporary: need a way to export an IPA file without XCode
+                        .onLongPressGesture(minimumDuration: 0.5, maximumDistance: 10) { 
+                            isExportSheetPresented = true
+                        }
+                    
+                }
+                .sheet(isPresented: $isSheetPresented, onDismiss: {}) {
+                    NewFavorSheet() // The sliding sheet content
+                }
+                .sheet(isPresented: $isExportSheetPresented, onDismiss: {}) {
+                    ExportView()
+                }
+                .toolbarBackground(.ultraThinMaterial, for: .tabBar)
             }
-            .sheet(isPresented: $isSheetPresented, onDismiss: {}) {
-                NewFavorSheet() // The sliding sheet content
-            }
-            .sheet(isPresented: $isExportSheetPresented, onDismiss: {}) {
-                ExportView()
-            }
-            .toolbarBackground(.ultraThinMaterial, for: .tabBar)
-                    }
+            
+            // Invisible Button that covers the middle tab, used to open the "New Favor" sheet
+            Button(action: {
+                isSheetPresented = true // Show the sheet when the tab is selected
+            }, label: {
+                Rectangle()
+                    .fill(.clear)
+            })
+            .frame(width: 120, height: 50)
+        }
         .tint(.blue)
    }
 }
