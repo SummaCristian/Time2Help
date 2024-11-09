7a45e695850c8e63ecc9c070b91f24b98cc042da
Cristian Summa <cristian.summa@mail.polimi.it>
2024-11-09 23:38:52 +0100

    Updated CircularProgressView
    - Added size support: using 'size:' it is possible to choose between 4 sizes: Small, Medium (default), Large, ExtraLarge
    - Added the ability to have the percentage displayed as a number inside, using 'showNumber:'
    - Added the ability to customize the colors using 'numberColor:' and 'progressColor:'

--- Views/CircularProgressView.swift
+++ Views/CircularProgressView.swift
@@ -3,27 +3,85 @@ import SwiftUI
 struct CircularProgressView: View {
     @State var value: Double
     
+    @State var showNumber: Bool = true
+    
+    @State var size: CircularProgressSize? = .medium
+    
+    @State var numberColor: Color?
+    @State var progressColor: Color?
+    
     var body: some View {
         ZStack {
             Circle()
                 .stroke(
                     Material.ultraThin,
-                    lineWidth: 5
+                    lineWidth: size!.lineWidth
                 )
-                .frame(width: 45, height: 45)
+                .frame(width: size?.value, height: size?.value)
             Circle()
                 .trim(from: 0, to: value)
                 .stroke(
-                    Color.green,
-                    // 1
+                    progressColor?.gradient ?? Color.green.gradient,
                     style: StrokeStyle(
-                        lineWidth: 5,
+                        lineWidth: size!.lineWidth,
                         lineCap: .round
                     )
                 )
                 .rotationEffect(.degrees(-90))
-                .frame(width: 45, height: 45)
+                .frame(width: size?.value, height: size?.value)
+            
+            if showNumber {
+                Text(String(Int(value * 100)))
+                    .font(.system(size: size!.textValue, weight: .bold, design: .rounded))
+                    .foregroundStyle(numberColor ?? .primary)
+            }
         }
-        .shadow(radius: 1)
+        .shadow(radius: 2)
+    }
+}
+
+enum CircularProgressSize {
+    case small
+    case medium
+    case large
+    case extraLarge
+    
+    var value: CGFloat {
+        switch self {
+            case .small:
+                return 30
+            case .medium:
+                return 45
+            case .large:
+                return 60
+            case .extraLarge:
+                return 80
+        }
+    }
+    
+    var textValue: CGFloat {
+        switch self {
+        case .small:
+            return 10
+        case .medium:
+            return 15
+        case .large:
+            return 25
+        case .extraLarge:
+            return 35
+        }
+    }
+    
+    var lineWidth: CGFloat {
+        switch self {
+        case .small:
+            return 2
+        case .medium:
+            return 5
+        case .large:
+            return 6
+        case .extraLarge:
+            return 7
+        }
     }
 }
