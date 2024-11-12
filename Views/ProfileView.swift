37fe6c76d3d24d78326b4a99429e8d329db466af
mattiaDina <mattia.dinardo@mail.polimi.it>
2024-11-12 18:17:45 +0100

    Updated ProfileView
    Modified the look of name and icon section
    
    Added nameSurname, selectedColor and selectedNeighbourhood binding variables to show the variables saved in memory of the app
    
    Added ModifyProfileView to pop up when "Modifica" button is clicked
    
    Added a padding to make a better view when the view is scrolled down

--- Views/ProfileView.swift
+++ Views/ProfileView.swift
@@ -6,53 +6,80 @@ struct ProfileView: View {
     // The Database, where the Favors are stored
     @ObservedObject var database: Database
     
-    // An Optional<Favor> used as a selector for a Favor: 
+    // An Optional<Favor> used as a selector for a Favor:
     // nil => no Favor selected
     // *something* => that Favor is selected
     @Binding var selectedFavor: Favor?
+    
+    @Binding var nameSurname: String
+    @Binding var selectedColor: String
+    @Binding var selectedNeighbourhood: String
         
     // Boolean value to handle the behavior of the "Export IPA" sheet
-    @State private var isExportSheetPresented = false
+    @State private var isExportSheetPresented: Bool = false
     
-    @State private var isChangeNameAlertPresented = false
+    @State private var isModifySheetPresented: Bool = false
     
     @State private var newName: String = ""
     
+    @State private var nameSurnameTemp: String = ""
+    @State private var selectedColorTemp: String = ""
+    @State private var selectedNeighbourhoodTemp: String = ""
+    @State private var errorNameSurnameEmpty: Bool = false
+    
     var body: some View {
         NavigationStack {
-            Form {
+            Form { //
                 // Profile View
-                HStack(spacing: 20) {
-                    // Profile picture
-                    ZStack {
-                        // Background
-                        Circle()
-                            .frame(width: 100, height: 100)
-                            .foregroundStyle(.cyan.gradient)
+                VStack(spacing: 20) {
+                    VStack(spacing: 16) {
+                        // Profile picture
+                        ZStack {
+                            // Background
+                            Circle()
+                                .frame(width: 110, height: 110)
+                                .foregroundStyle(.background)
+                                .overlay {
+                                    Circle()
+                                        .stroke(.gray, lineWidth: 0.5)
+                                }
+                            
+                            Circle()
+                                .frame(width: 100, height: 100)
+                                .foregroundStyle(selectedColor.toColor().gradient)
+                            
+                            // Icon
+                            Text(nameSurname.filter({ $0.isUppercase }))
+                                .font(.custom("Futura-bold", size: nameSurname.filter({ $0.isUppercase }).count == 1 ? 35 : nameSurname.filter({ $0.isUppercase }).count == 2 ? 30 : 25))
+                                .foregroundStyle(.white)
+                        }
+                        // Temporary: need a way to export an IPA file without XCode
+                        .onLongPressGesture(minimumDuration: 0.5, maximumDistance: 10) {
+                            isExportSheetPresented = true
+                        }
                         
-                        // Icon
-                        Image(systemName: "person.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 40, height: 40)
-                            .foregroundStyle(.white)
-                    }
-                    // Temporary: need a way to export an IPA file without XCode
-                    .onLongPressGesture(minimumDuration: 0.5, maximumDistance: 10) { 
-                        isExportSheetPresented = true
+                        // Info
+                        Text(nameSurname)
+                            .font(.title.bold())
+                        
+                        Text("Quartiere: " + selectedNeighbourhood)
+                            .font(.body)
+                            .foregroundStyle(.gray)
+                            .padding(.top, -12)
                     }
                     
-                    // Info
-                    VStack() {
-                        Text(database.userName)
-                            .font(.title2)
-                            .bold()
-                            .onLongPressGesture() {
-                                isChangeNameAlertPresented = true
-                            }
-                        
-                        Spacer()
-                            .frame(height: 40)
+                    Button {
+                        isModifySheetPresented = true
+                    } label: {
+                        HStack(spacing: 8) {
+                            Image(systemName: "pencil")
+                            Text("Modifica")
+                        }
+                        .font(.body.bold())
+                        .foregroundStyle(.white)
+                        .padding(.vertical, 15)
+                        .frame(maxWidth: .infinity)
+                        .background(selectedColor.toColor(), in: .capsule)
                     }
                 }
                 .listRowBackground(Color.clear)
@@ -60,11 +87,11 @@ struct ProfileView: View {
                 // Rewards Section
                 Section(
                     content: {
-                        HStack {}
+                        HStack { }
                     },
                     header: {
                         Text("Riconoscimenti")
-                    }, 
+                    },
                     footer: {
                         Text("Colleziona le medaglie completando dei Favori")
                     }
@@ -74,7 +101,7 @@ struct ProfileView: View {
                 Section(
                     content: {
                         // Favors
-                        LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 30), count: 2), content: {
+                        LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 2), content: {
                             ForEach(database.favors) { favor in
                                 FavorBoxView(favor: favor)
                                     .onTapGesture {
@@ -87,7 +114,8 @@ struct ProfileView: View {
                         Text("I tuoi favori")
                     },
                     footer: {
-                        Text("Monitora lo stato dei tuoi Favori ancora in corso")
+                        Text("Monitora lo stato dei Tuoi Favori ancora in corso")
+                            .safeAreaPadding(.bottom, 40)
                     }
                 )
             }
@@ -96,22 +124,14 @@ struct ProfileView: View {
         .sheet(isPresented: $isExportSheetPresented, onDismiss: {}) {
             ExportView()
         }
-        .alert("Cambia nome", 
-               isPresented: $isChangeNameAlertPresented, 
-               actions: {
-                    Button("Annulla", role: .destructive, action: {
-                        isChangeNameAlertPresented = false
-                        }
-                    )
-                    
-                    Button("Conferma", role: .cancel, action: {
-                        database.userName = newName
-                        isChangeNameAlertPresented = true
-                        }
-                    )
-            
-            TextField("Nome Cognome", text: $newName)
-               }
-           )
+        .sheet(isPresented: $isModifySheetPresented, content: {
+            ModifyProfileView(isModifySheetPresented: $isModifySheetPresented, nameSurname: $nameSurname, selectedColor: $selectedColor, selectedNeighbourhood: $selectedNeighbourhood, nameSurnameTemp: $nameSurnameTemp, selectedColorTemp: $selectedColorTemp, selectedNeighbourhoodTemp: $selectedNeighbourhoodTemp)
+                .interactiveDismissDisabled()
+        })
+        .onAppear {
+            nameSurnameTemp = nameSurname
+            selectedColorTemp = selectedColor
+            selectedNeighbourhoodTemp = selectedNeighbourhood
+        }
     }
 }
