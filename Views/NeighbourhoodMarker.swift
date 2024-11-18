e096ce8c86c8ca5166cd4fb0d6c2f723ad12ce94
mattiaDina <mattia.dinardo@mail.polimi.it>
2024-11-18 20:18:13 +0100

    Update NeighbourhoodMarker.swift
    Changed the look of neighbourhood markers

--- Views/NeighbourhoodMarker.swift
+++ Views/NeighbourhoodMarker.swift
@@ -11,21 +11,27 @@ struct NeighbourhoodMarker: View {
         VStack(spacing: 10) {
             Text("Q")
                 .font(.custom("Futura-bold", size: 25))
-                .foregroundStyle(isSelected ? .white : .orange)
+//                .foregroundStyle(isSelected ? .white : .orange)
+                .foregroundStyle(LinearGradient(colors: isSelected ? (colorScheme == .dark ? [Color(.systemGray3), Color(.systemGray5)] : [Color(.white), Color(.systemGray5)]) : [.orange], startPoint: .topLeading, endPoint: .bottomTrailing))
                 .frame(width: 50, height: 50)
                 .background(
                     RoundedRectangle(cornerRadius: 12)
-                        .foregroundStyle(isSelected ? LinearGradient(colors: [.orange], startPoint: .topLeading, endPoint: .bottomTrailing) : LinearGradient(colors: colorScheme == .dark ? [Color(.systemGray4), Color(.systemGray5)] : [Color(.systemGray6), Color(.systemGray5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .foregroundStyle(LinearGradient(colors: isSelected ? [.orange] : (colorScheme == .dark ? [Color(.systemGray4), Color(.systemGray5)] : [Color(.systemGray6), Color(.systemGray5)]), startPoint: .topLeading, endPoint: .bottomTrailing))
                         .shadow(color: .primary.opacity(0.2), radius: 6)
                         .padding(.all, isSelected ? -4 : 0)
                         .overlay {
                             RoundedRectangle(cornerRadius: isSelected ? 8 : 12)
-                                .stroke(isSelected ? LinearGradient(colors: [.white], startPoint: .topLeading, endPoint: .bottomTrailing) : LinearGradient(colors: colorScheme == .dark ? [Color(.systemGray3), Color(.systemGray5)] : [Color(.white), Color(.systemGray5)], startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 2)
+                                .stroke(LinearGradient(colors: colorScheme == .dark ? [Color(.systemGray3), Color(.systemGray5)] : [Color(.white), Color(.systemGray5)], startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: isSelected ? 3 : 2)
                         }
                 )
-            
-            Text(neighbourhood.name)
-                .font(.subheadline.bold())
+                
+            if neighbourhood.big || isSelected {
+                Text(neighbourhood.name)
+                    .font(.subheadline.bold())
+            }
         }
+        .scaleEffect(neighbourhood.big || isSelected ? 1 : 0.4)
+        .animation(.spring(duration: 0.2), value: isSelected)
+        .animation(.spring(duration: 0.2), value: neighbourhood.big)
     }
 }
