dc7f94f67138a9d0fe18b66842b471739cb1603c
mattiaDina <mattia.dinardo@mail.polimi.it>
2024-11-18 20:13:43 +0100

    Update MapNeighbourhoodSelectorView.swift
    Changed the entire look of the selector to ensure that the map only shows with larger markers the markers close to the center of the camera and the selected one. Furthermore, depending on the zoom on the map, you can see more or fewer markers
    
    Added the possibility of selecting the neighborhood through a list and not through the map
    
    Changed the drag indicator from visible to hidden
    
    Changedt he definition of N Neighborhood to store "big" bool variable

--- Views/MapNeighbourhoodSelectorView.swift
+++ Views/MapNeighbourhoodSelectorView.swift
@@ -10,78 +10,180 @@ struct NeighbourhoodSelector: View {
     @Binding var selectedNeighbourhoodStructTemp: Neighbourhood
     @Binding var selectedNeighbourhoodStructTempTwo: Neighbourhood
     
-    // The newly created favor, whose location is being edited in here
-    //    @ObservedObject var newFavor: Favor
+    @State private var neighbourhoodsUpdated: [Neighbourhood] = Database.neighbourhoods
+    
+    @State private var camera: MapCameraPosition = MapCameraPosition.automatic
+    
+    @State private var showList: Bool = false
     
     // The UI
     var body: some View {
         ZStack {
-            // The MapReader is needed to intercept the User's tap inputs
-            MapReader { reader in
-                Map(
-                    initialPosition: MapCameraPosition.camera(.init(centerCoordinate: selectedNeighbourhoodStructTemp.location, distance: 3200)),
-                    bounds: MapCameraBounds(minimumDistance: 500, maximumDistance: .infinity),
-                    interactionModes: .all
-                ) {
-                    // The User's Location
-                    UserAnnotation()
-                    
-                    // The Favor's Marker
-                    // Note: it's set in selectedLocation if possible, but if it's nil, it defaults to the Favor's old Location
-                    ForEach(Database.neighbourhoods) { neighbourhood in
-                        Annotation("", coordinate: neighbourhood.location) {
-                            NeighbourhoodMarker(isSelected: .constant(neighbourhood.id == selectedNeighbourhoodStructTempTwo.id), neighbourhood: neighbourhood)
-                                .onTapGesture {
-                                    selectedNeighbourhoodStructTempTwo = neighbourhood
+            Map(
+                position: $camera,
+                bounds: MapCameraBounds(minimumDistance: 500, maximumDistance: .infinity),
+                interactionModes: .all
+            ) {
+                // The User's Location
+                UserAnnotation()
+                
+                // The Favor's Marker
+                // Note: it's set in selectedLocation if possible, but if it's nil, it defaults to the Favor's old Location
+                ForEach(neighbourhoodsUpdated) { neighbourhood in
+                    Annotation("", coordinate: neighbourhood.location) {
+                        NeighbourhoodMarker(isSelected: .constant(neighbourhood.id == selectedNeighbourhoodStructTempTwo.id), neighbourhood: neighbourhood)
+                            .onTapGesture {
+                                selectedNeighbourhoodStructTempTwo = neighbourhood
+                                withAnimation(.spring(duration: 0.2)) {
+                                    camera = MapCameraPosition.camera(.init(centerCoordinate: .init(latitude: neighbourhood.location.latitude, longitude: neighbourhood.location.longitude), distance: camera.camera?.distance ?? 7200))
                                 }
-                        }
+                            }
                     }
                 }
-                .mapControlVisibility(.visible)
-                .mapControls {
-                    MapUserLocationButton()
-                    MapCompass()
-                    MapScaleView()
-                    MapPitchToggle()
-                }
-                .mapStyle(
-                    .standard(
-                        elevation: .realistic,
-                        emphasis: .automatic,
-                        pointsOfInterest: .excludingAll
-                    )
+            }
+            .mapControlVisibility(.visible)
+            .mapControls {
+                MapUserLocationButton()
+                MapCompass()
+                MapScaleView()
+                MapPitchToggle()
+            }
+            .mapStyle(
+                .standard(
+                    elevation: .realistic,
+                    emphasis: .automatic,
+                    pointsOfInterest: .excludingAll
                 )
-                .safeAreaPadding(.top, 110)
-                .safeAreaPadding(.leading, 5)
-                .safeAreaPadding(.trailing, 10)
-                .safeAreaPadding(.bottom, 80)
+            )
+            .onAppear {
+                camera = MapCameraPosition.camera(.init(centerCoordinate: selectedNeighbourhoodStructTemp.location, distance: 3200))
+            }
+            .onMapCameraChange(frequency: .onEnd) { camera in
+                let centerCoordinate = camera.camera.centerCoordinate
+                neighbourhoodsUpdated = neighbourhoodsUpdated.sorted(by: {
+                    let distanceZero = sqrt(pow(centerCoordinate.latitude - $0.location.latitude, 2) + pow(centerCoordinate.longitude - $0.location.longitude, 2))
+                    let distanceOne = sqrt(pow(centerCoordinate.latitude - $1.location.latitude, 2) + pow(centerCoordinate.longitude - $1.location.longitude, 2))
+                    
+                    return distanceZero < distanceOne
+                })
+                let cameraDistance = camera.camera.distance
+                let numberOfBigs = cameraDistance < 10000 ? 5 : cameraDistance < 15000 ? 3 : cameraDistance < 30000 ? 1 : 0
+                for i in 0..<numberOfBigs {
+                    if !neighbourhoodsUpdated[i].big {
+                        neighbourhoodsUpdated[i].big = true
+                    }
+                }
+                for j in numberOfBigs..<neighbourhoodsUpdated.count {
+                    if neighbourhoodsUpdated[j].big {
+                        neighbourhoodsUpdated[j].big = false
+                    }
+                }
+                self.camera = MapCameraPosition.camera(.init(centerCoordinate: .init(latitude: centerCoordinate.latitude, longitude: centerCoordinate.longitude), distance: camera.camera.distance))
+            }
+            .safeAreaPadding(.top, 110)
+            .safeAreaPadding(.leading, 5)
+            .safeAreaPadding(.trailing, 10)
+            .safeAreaPadding(.bottom, 80)
+            
+            if showList {
+                Rectangle()
+                    .foregroundStyle(.thinMaterial)
+                    .safeAreaPadding(.top, 97)
+                    .edgesIgnoringSafeArea(.bottom)
+                
+                ScrollView {
+                    VStack(spacing: 20) {
+                        ForEach(Database.neighbourhoods) { neighbourhood in
+                            Button {
+                                selectedNeighbourhoodStructTempTwo = neighbourhood
+                                withAnimation(.spring(duration: 0.2)) {
+                                    camera = MapCameraPosition.camera(.init(centerCoordinate: .init(latitude: neighbourhood.location.latitude, longitude: neighbourhood.location.longitude), distance: camera.camera?.distance ?? 7200))
+                                }
+                                showList = false
+                            } label: {
+                                HStack {
+                                    Text(neighbourhood.name)
+                                        .foregroundStyle(colorScheme == .dark ? .white : .black)
+                                    
+                                    Spacer()
+                                }
+                                .overlay(alignment: .trailing) {
+                                    if neighbourhood.id == selectedNeighbourhoodStructTempTwo.id {
+                                        Image(systemName: "checkmark.circle.fill")
+                                            .font(.title2.bold())
+                                            .foregroundStyle(.green)
+                                    }
+                                }
+                                .padding(.all, 20)
+                                .background(colorScheme == .dark ? Color(.systemGray6) : .white, in: .rect(cornerRadius: 16))
+                            }
+                        }
+                    }
+                    .padding(.all, 20)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(colorScheme == .dark ? .black : Color(.systemGray6), in: .rect)
+                .safeAreaPadding(.top, 97)
+                .padding(.bottom, 90)
+                
+//                VStack {
+//                    Spacer()
+//
+//
+//                        .frame(height: 90)
+//                        .ignoresSafeArea()
+//                }
             }
             
             // The Title bar
             VStack {
-                VStack {
-                    // Some space at the top
-                    Spacer()
-                        .frame(height: 10)
+//                VStack {
+//                    // Some space at the top
+//                    Spacer()
+//                        .frame(height: 10)
                     
                     // The Text
                     HStack(spacing: 8) {
                         Text("Clicca sul marcatore per selezionare il tuo quartiere")
                             .font(.headline)
                         
-                        
                         Spacer()
                         
-                        NeighbourhoodMarker(isSelected: .constant(false), neighbourhood: .init(name: "", location: .init()))
-                            .padding(.bottom, -20)
+                        NeighbourhoodMarker(isSelected: .constant(false), neighbourhood: .init(name: "", location: .init(), big: true))
+                            .padding(.bottom, -25)
                     }
-                    .padding(.vertical, 20)
+                    .padding(.vertical, 25)
                     .padding(.horizontal, 20)
-                }
+//                }
                 .background(.ultraThinMaterial)
                 
                 Spacer()
             }
+            
+//            Text("\(altitude)")
+            VStack(alignment: .leading) {
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        showList.toggle()
+                    } label: {
+                        Image(systemName: showList ? "map.fill" : "magnifyingglass")
+                            .font(.title3.bold())
+                            .foregroundStyle(.white)
+                            .padding(.all, 12)
+                    }
+                    .background(
+                        Circle()
+                            .foregroundStyle(.blue)
+                    )
+                }
+                
+                Spacer()
+            }
+            .safeAreaPadding(.top, 115)
+            .safeAreaPadding(.trailing, showList ? 20 : 70)
+            .animation(.spring(duration: 0.4), value: showList)
             
             // The bottom Buttons
             VStack {
@@ -91,7 +193,9 @@ struct NeighbourhoodSelector: View {
                     // The Cancel Button
                     // This button does not save the newly selectedLocation
                     Button(action: {
-                        selectedNeighbourhoodStructTempTwo = selectedNeighbourhoodStructTemp
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                            selectedNeighbourhoodStructTempTwo = selectedNeighbourhoodStructTemp
+                        }
                         // Dismisses the sheet
                         dismiss()
                     }) {
@@ -126,7 +230,7 @@ struct NeighbourhoodSelector: View {
             }
             .padding(.all, 20)
         }
-        .presentationDragIndicator(.visible)
+        .presentationDragIndicator(.hidden)
     }
 }
 
@@ -134,9 +238,17 @@ struct Neighbourhood: Identifiable {
     var id: UUID = UUID()
     var name: String
     var location: CLLocationCoordinate2D
+    var big: Bool
     
     init(name: String, location: CLLocationCoordinate2D) {
         self.name = name
         self.location = location
+        self.big = false
+    }
+    
+    init(name: String, location: CLLocationCoordinate2D, big: Bool) {
+        self.name = name
+        self.location = location
+        self.big = big
     }
 }
