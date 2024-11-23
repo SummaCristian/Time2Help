cef5133784d18070dd40ea06ead7f1e60c98dac5
mattiaDina <mattia.dinardo@mail.polimi.it>
2024-11-24 00:31:19 +0100

    Update MyApp.swift
    Changed the initial value of the selected neighborhood
    
    Moved the viewModel to this page to be able to share the same model between the login pages and the actual app pages

--- MyApp.swift
+++ MyApp.swift
@@ -6,14 +6,16 @@ struct AppTime2Help2App: App {
     @AppStorage("showlogin") var showLogin: Bool = true
     @AppStorage("nameSurname") var nameSurname: String = ""
     @AppStorage("selectedColor") var selectedColor: String = "blue"
-    @AppStorage("selectedNeighbourhood") var selectedNeighbourhood: String = "Città Studi"
+    @AppStorage("selectedNeighbourhood") var selectedNeighbourhood: String = "Duomo"
+    
+    @StateObject private var viewModel = MapViewModel()
     
     var body: some Scene {
         WindowGroup {
             if showLogin {
-                LoginView(showLogin: $showLogin, nameSurname: $nameSurname, selectedColor: $selectedColor, selectedNeighbourhood: $selectedNeighbourhood)
+                LoginView(showLogin: $showLogin, viewModel: viewModel, nameSurname: $nameSurname, selectedColor: $selectedColor, selectedNeighbourhood: $selectedNeighbourhood)
             } else {
-                ContentView(nameSurname: $nameSurname, selectedColor: $selectedColor, selectedNeighbourhood: $selectedNeighbourhood)
+                ContentView(viewModel: viewModel, nameSurname: $nameSurname, selectedColor: $selectedColor, selectedNeighbourhood: $selectedNeighbourhood)
             }
         }
     }
