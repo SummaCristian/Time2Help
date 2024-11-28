dbed24f37d0c4ac705057b203acd5c18fc4a6f5d
Cristian Summa <cristian.summa@mail.polimi.it>
2024-11-28 23:40:31 +0100

    Inserted AdvancedFiltersView in the UI
    A Button is placed inside CategoryFiltersView to show the Advanced Filters popup.
    
    This popup's appearance and disappearance are animated.

--- Views/CategoryFiltersView.swift
+++ Views/CategoryFiltersView.swift
@@ -1,64 +1,133 @@
 import SwiftUI
 
 struct CategoryFiltersView: View {
-    @Binding var selectedCategories: [FavorCategory]
+    @Namespace private var animationNamespace
+    
+    @ObservedObject var filter: FilterModel
+    
+    @State private var isAdvancedFiltersViewShowing = false
+    
+    @State private var scrollingPosition: Int?
+    
+    // For animation
+    @State private var moveToCenter = false
+    @State private var blurContainer = true
+    @State private var startClosing = false
     
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 10) {
-                ForEach(FavorCategory.allCases) { category in
-                    CategoryCapsule(selectedCategories: $selectedCategories, category: category)
-                        .onTapGesture {
-                            if category == .all {
-                                // The "Tutte" capsule
-                                selectCategory(category: .all)
-                            } else {
-                                // The real categories
-                                if selectedCategories.contains(category) {
-                                    deselectCategory(category: category)
-                                } else {
-                                    selectCategory(category: category)
+        GeometryReader { geometry in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 10) {
+                    // Advanced Filters Button
+                    ZStack {
+                        if !isAdvancedFiltersViewShowing {
+                            Button {
+                                var transaction = Transaction(animation: .smooth)
+                                transaction.disablesAnimations = false
+                                
+                                withTransaction(transaction) {
+                                    moveToCenter = true
                                 }
+                                
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                    withTransaction(transaction) {
+                                        moveToCenter = false
+                                        isAdvancedFiltersViewShowing.toggle()
+                                    }
+                                }
+                                
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                                    withTransaction(transaction) {
+                                        blurContainer = false
+                                    }
+                                }
+                                
+                            } label: {
+                                Image(systemName: "line.3.horizontal.decrease")
+                                    .font(.system(size: 20, weight: .medium))
+                                    .padding(10)
+                                    .foregroundStyle(.primary)
                             }
+                            .scaleEffect(moveToCenter ? 1.5 : 1)
+                            .clipShape(Circle())
+                            .hoverEffect(.lift)
+                            .matchedGeometryEffect(id: "advancedFilters", in: animationNamespace)
+                        } else {
+                            AdvancedFiltersView(
+                                screenWidth: geometry.size.width,
+                                screenHeight: geometry.size.height,
+                                filter: filter,
+                                isAdvancedFiltersViewShowing: $startClosing
+                            )
+                            .matchedGeometryEffect(id: "advancedFilters", in: animationNamespace)
+                            .clipShape(RoundedRectangle(cornerRadius: 32))
+                            .blur(radius: blurContainer ? 10 : 0)
+                            .padding(5)
                         }
+                    }
+                    .frame(minHeight: 60)
+                    .background(.thinMaterial)
+                    .clipShape(isAdvancedFiltersViewShowing ? AnyShape(RoundedRectangle(cornerRadius: 32)) : AnyShape(Circle()))
+                    .offset(
+                        x: moveToCenter ? 130 : 0, 
+                        y: moveToCenter ? 200 : 0)
+                    .shadow(radius: 3)
+                    
+                    HStack(spacing: 10) {
+                        // Real Categories
+                        ForEach(FavorCategory.allCases) { category in
+                            CategoryCapsule(filter: filter, category: category)
+                                .onTapGesture {
+                                    if category == .all {
+                                        // The "Tutte" capsule
+                                        filter.selectCategory(category: .all)
+                                    } else {
+                                        // The real categories
+                                        if filter.selectedCategories.contains(category) {
+                                            filter.deselectCategory(category: category)
+                                        } else {
+                                            filter.selectCategory(category: category)
+                                        }
+                                    }
+                                }
+                        }
+                    }
+                    .padding(.vertical, 8)
                 }
+                .padding(.horizontal)
+                .frame(minHeight: 500, alignment: .top)
             }
-            .padding(.horizontal)
+            .scrollDisabled(isAdvancedFiltersViewShowing)
         }
-    }
-    
-    func selectCategory(category: FavorCategory) {
-        if category != .all {
-            if selectedCategories.contains(.all) {
-                // Selects onlt the clicked category
-                selectedCategories.removeAll()
-            }
-            if !selectedCategories.contains(category) {
-                if selectedCategories.count == FavorCategory.allCases.count - 2 {
-                    // All Categories have been selected, switch to .all
-                    selectedCategories.removeAll()
-                    selectedCategories.append(.all)
-                } else {
-                    // Adds the current category to the selection
-                    selectedCategories.append(category)
+        .sensoryFeedback(.impact, trigger: moveToCenter, condition: { old, new in
+            new
+        })
+        .sensoryFeedback(.impact, trigger: startClosing, condition: { old, new in
+            new
+        })
+        .sensoryFeedback(.levelChange, trigger: filter.maxDuration)
+        
+        .onChange(of: startClosing) { _, _ in
+            if startClosing {
+                var transaction = Transaction(animation: .smooth)
+                transaction.disablesAnimations = false
+                
+                withTransaction(transaction) {
+                    blurContainer = true
+                    moveToCenter = true
                 }
-            }
-        } else {
-            selectedCategories.removeAll()
-            selectedCategories.append(.all)
-        }
-    }
-    
-    func deselectCategory(category: FavorCategory) {
-        if category != .all {
-            if selectedCategories.contains(category) {
-                if selectedCategories.count > 1 {
-                    // There are still selected categories excluding this one
-                    selectedCategories.removeAll {$0 == category}
-                } else {
-                    // This one was the only one selected, returning to .all
-                    selectedCategories.removeAll()
-                    selectedCategories.append(.all)
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    withTransaction(transaction) {
+                        isAdvancedFiltersViewShowing.toggle()
+                    }
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    withTransaction(transaction) {
+                        moveToCenter = false
+                        startClosing = false
+                    }
                 }
             }
         }
@@ -67,7 +136,8 @@ struct CategoryFiltersView: View {
 
 #Preview {
     VStack(spacing: 50) {
-        CategoryFiltersView(selectedCategories: .constant([.all]))
-        CategoryFiltersView(selectedCategories: .constant([]))
+        CategoryFiltersView(filter: FilterModel())
+        CategoryFiltersView(filter: FilterModel(selectedCategories: [])
+        )
     }
 }
