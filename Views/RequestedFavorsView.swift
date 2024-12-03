ebe952972a655631205c0df66472a5c3b89a30a5
Cristian Summa <cristian.summa@mail.polimi.it>
2024-12-03 15:43:34 +0100

    Added RequestedFavorsView

---
+++ Views/RequestedFavorsView.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+
+struct RequestedFavorsView: View {
+    @ObservedObject var viewModel: MapViewModel
+    
+    // The Database, where the Favors are stored
+    @ObservedObject var database: Database
+    
+    @Binding var user: User
+    
+    // An Optional<Favor> used as a selector for a Favor:
+    // nil => no Favor selected
+    // *something* => that Favor is selected
+    @State var selectedFavor: Favor? = nil
+    
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                // Requested Favors
+                Section(
+                    content: {
+                        // Favors
+                        if (database.favors.filter({ $0.author.id == user.id }).count) == 0 {
+                            Text("Nessun Favore richiesto ...")
+                        } else {
+                            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 2)) {
+                                ForEach(database.favors.filter({ $0.author.id == user.id })) { favor in
+                                    FavorBoxView(favor: favor)
+                                        .onTapGesture {
+                                            selectedFavor = favor
+                                        }
+                                }
+                            }
+                            .padding(.vertical, 2)
+                        }
+                    },
+                    header: {
+                        Text("I tuoi favori")
+                    },
+                    footer: {
+                        Text("Monitora lo stato dei Favori che hai richiesto")
+                            .safeAreaPadding(.bottom, 40)
+                    }
+                )
+            }
+        }
+        .navigationTitle("I tuoi Favori")
+        .navigationDestination(item: $selectedFavor) { favor in
+            FavorDetailsSheet(viewModel: viewModel, database: database, selectedFavor: $selectedFavor, user: user, favor: favor)
+        }
+    }
+}
