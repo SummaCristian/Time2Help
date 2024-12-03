7271323d57cfce95b7dff2afd7b5cf6c265ea827
Cristian Summa <cristian.summa@mail.polimi.it>
2024-12-03 23:16:18 +0100

    Added the Reward's Details
    An Overlay covers the whole screen when the User taps on one of its Rewards.
    
    The overlay is located inside ContentView, so it covers the whole UI, TabView included

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -4,6 +4,8 @@ import MapKit
 struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme
     
+    @Namespace private var rewardNameSpace
+        
     // Connection to the ViewModel for Data and Location Permissions
     @ObservedObject var viewModel: MapViewModel
     
@@ -26,6 +28,11 @@ struct ContentView: View {
     // Boolean value to hanlde the behavior of the "New Favor" sheet
     @State private var isSheetPresented = false // State to control sheet visibility
     
+    @State private var selectedReward: Reward?
+    
+    @State private var reduceRewardBadge = false
+    @State private var showRewardTexts = false
+    
     // Main View
     var body: some View {
         // The main UI of this app is composed of a TabView, meaning the the UI is divided
@@ -36,7 +43,7 @@ struct ContentView: View {
                 TabView(selection: $selectedTab) {
                     Group {
                         // Tab 0: Map
-                        MapView(viewModel: viewModel, database: database, selectedFavor: $selectedFavor, selectedFavorID: $selectedFavorID, selectedNeighbourhood: selectedNeighbourhood)
+                        MapView(viewModel: viewModel, database: database, selectedFavor: $selectedFavor, selectedFavorID: $selectedFavorID, selectedNeighbourhood: selectedNeighbourhood, user: $user)
                             .tabItem {
                                 Label("Mappa", systemImage: "map")
                             }
@@ -51,7 +58,7 @@ struct ContentView: View {
                             .disabled(true)
                         
                         // Tab 2: Profile
-                        ProfileView(viewModel: viewModel, database: database, selectedFavor: $selectedFavor, user: $user, isEditable: true)
+                        ProfileView(viewModel: viewModel, database: database, selectedFavor: $selectedFavor, user: $user, selectedReward: $selectedReward, rewardNameSpace: rewardNameSpace, isEditable: true)
                             .tabItem {
                                 Label("Profilo", systemImage: "person.fill")
                             }
@@ -92,14 +99,89 @@ struct ContentView: View {
                 selectedFavorID = nil
             },
             content: { favor in
-                FavorDetailsSheet(viewModel: viewModel, database: database, selectedFavor: $selectedFavor, user: user, favor: favor)
+                FavorDetailsSheet(viewModel: viewModel, database: database, selectedFavor: $selectedFavor, user: user, favor: favor, selectedReward: $selectedReward, rewardNameSpace: rewardNameSpace)
             })
         .toolbarBackground(.ultraThinMaterial, for: .tabBar)
+        // Reward Details Overlay
+        .overlay {
+            if selectedReward != nil {
+                ZStack {
+                    Rectangle()
+                        .foregroundStyle(.ultraThinMaterial)
+                        .overlay {
+                            // Background Gradient
+                            LinearGradient(colors: [selectedReward!.color.opacity(0.4), selectedReward!.color], startPoint: .topLeading, endPoint: .bottomTrailing)
+                                .opacity(0.4)
+                        }
+                        .ignoresSafeArea()
+
+                    VStack(spacing: 40) {
+                        Spacer().frame(height: 100)
+                        
+                        // Reward Icon
+                        selectedReward!.rewardView
+                            .matchedGeometryEffect(id: selectedReward!.id, in: rewardNameSpace)
+                            .scaleEffect(reduceRewardBadge ? 0.5 : 2.2)
+                            .padding(.bottom, 50)
+                        
+                        // Reward Title
+                        Text(selectedReward!.title)
+                            .font(.title)
+                            .fontWeight(.heavy)
+                            .fontDesign(.rounded)
+                            .foregroundStyle(
+                                LinearGradient(
+                                    gradient: Gradient( colors: [.indigo, .purple, .pink, .red, .orange, .yellow, .green, .cyan]), 
+                                    startPoint: .topLeading, 
+                                    endPoint: .bottomTrailing
+                                )
+                            )
+                            .shadow(radius: 5)
+                            .scaleEffect(showRewardTexts ? 1 : 0)
+                        
+                        // Reward description
+                        Text(selectedReward!.description)
+                            .scaleEffect(showRewardTexts ? 1 : 0)
+                        
+                        Spacer()
+                    }
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                            withAnimation {
+                                showRewardTexts = true
+                            }
+                        }
+                    }
+                    .onDisappear {
+                        showRewardTexts = false
+                    }
+                }
+                .onTapGesture {
+                    withAnimation {
+                        reduceRewardBadge = true
+                    }
+                    withAnimation(.interpolatingSpring) {
+                        selectedReward = nil
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        reduceRewardBadge = false
+                    }
+                }
+            }
+        }
         .ignoresSafeArea(.keyboard)
         .tint(.blue)
         .onAppear() {
             // Creates the main User
             user = User(nameSurname: $nameSurname, neighbourhood: $selectedNeighbourhood, profilePictureColor: $selectedColor)
+            
+            user.rewards.append(.numberTen)
+            user.rewards.append(.numberTwenty)
+            user.rewards.append(.numberFifty)
+            user.rewards.append(.numberHundred)
+            user.rewards.append(.gardeningCategory)
+            user.rewards.append(.jobCategory)
+            
             database.users.append(user)
             
             // Adds the template Markers to the Map
