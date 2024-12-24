bfc9d4ccee32af530a62f1158ea1bd55f161314d
Cristian Summa <cristian.summa@mail.polimi.it>
2024-12-24 17:55:47 +0100

    Added TimeSlotsList
    TimeSlotsList is a custom component designed to show and interact with a Favor's TimeSlots.
    
    This component has 2 variants:
    - An editable one, which allows to view and edit existing TimeSlots, to Add a new one, to Remove existing ones, or to Reorder them.
    * Delete is accessible by swiping right to left on a row
    ** Reordering is accessible by hold-dragging a row
    
    - A non editable one, which is view-only.
    * This variant groups all the TimeSlots within the same DAY in a single row.

---
+++ Views/TimeSlotsListView.swift
@@ -0,0 +1,140 @@
+import SwiftUI
+
+struct TimeSlotsList: View {
+    @Binding var slots: [TimeSlot]
+    
+    var isEditable: Bool = true
+    
+    @State var tint: Color = .accentColor
+    
+    var body: some View {
+        if isEditable {
+            List {
+                ForEach($slots) { slot in
+                    HStack {
+                        Spacer()
+                        
+                        DatePicker("", selection: slot.startingDate, displayedComponents: [.date, .hourAndMinute])
+                            .labelsHidden()
+                        
+                        Image(systemName: "arrow.right")
+                            .bold()
+                        
+                        DatePicker("", selection: slot.endingDate, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                        
+                        Spacer()
+                        
+                        Image(systemName: "line.horizontal.3")
+                            .bold()
+                            .foregroundStyle(.secondary)
+                    }
+                    .padding(5)
+                }
+                .onDelete(perform: { indexSet in
+                    slots.remove(atOffsets: indexSet)
+                })
+                .onMove(perform: { indices, newOffset in
+                    slots.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                
+                // Last button
+                HStack{
+                    Spacer()
+                    
+                    Button(
+                        action: {
+                            withAnimation {
+                                slots.append(TimeSlot(startingDate: Date(), endingDate: Date().addingTimeInterval(3600)))
+                            }
+                        }, label: {
+                            HStack {
+                                Image(systemName: "plus")
+                                    .bold()
+                                
+                                Text("Aggiungi fascia")
+                                    .bold()
+                            }
+                        })
+                    .buttonStyle(BorderedButtonStyle())
+                    .buttonBorderShape(.capsule)
+                    .padding(.vertical, 6)
+                }
+            }
+        } else {
+                // View only variant
+                List {
+                    // The unique days in which there exists a TimeSlot.
+                    // Each day will have its own Row, which is why we need to extract them
+                    let days: [Date] = extractDays()
+                    
+                    ForEach(days.indices) { index in
+                        HStack {
+                            // Day indication per-row
+                            Text(days[index].formatted(date: .long, time: .omitted))
+                                .bold()
+                                .foregroundStyle(tint)
+                            
+                            Spacer()
+                            
+                            // Time Slots
+                            VStack {
+                                let calendar = Calendar.current
+                                let slotsInDay = slots.filter({ calendar.isDate($0.startingDate, inSameDayAs: days[index])})
+                                
+                                ForEach(slotsInDay) { slot in 
+                                    HStack {
+                                        Text(slot.startingDate.formatted(date: .omitted, time: .shortened))
+                                            .foregroundStyle(tint)
+                                        
+                                        Image(systemName: "arrow.right")
+                                        
+                                        Text(slot.endingDate.formatted(date: .omitted, time: .shortened))
+                                            .foregroundStyle(tint)
+                                    }
+                                    .bold()
+                                }
+                            }
+                            .padding(.vertical)
+                        }
+                    }
+                    .frame(maxHeight: .infinity)
+                }
+            
+        }
+    }
+    
+    // Extracts the unique Days in which there is a TimeSlot
+    func extractDays() -> [Date] {
+        var days: [Date] = []
+        let calendar = Calendar.current
+        
+        for slot in slots {
+            if !(days.contains(where: { calendar.isDate($0, inSameDayAs: slot.startingDate)})) {
+                days.append(slot.startingDate)
+            }
+        }
+        
+        return days
+    }
+}
+
+#Preview {
+    VStack() {
+        // Editable
+        TimeSlotsList(slots: .constant(
+            [TimeSlot(startingDate: Date(), endingDate: Date().addingTimeInterval(3600)), 
+             TimeSlot(startingDate: Date(), endingDate: Date().addingTimeInterval(3600)), 
+             TimeSlot(startingDate: Date(), endingDate: Date().addingTimeInterval(3600)), 
+             TimeSlot(startingDate: Date(), endingDate: Date().addingTimeInterval(3600))
+            ]))
+        
+        // Non Editable
+        TimeSlotsList(slots: .constant(
+            [TimeSlot(startingDate: Date(), endingDate: Date().addingTimeInterval(3600)), 
+             TimeSlot(startingDate: Date().addingTimeInterval(36000), endingDate: Date().addingTimeInterval(36200)), 
+             TimeSlot(startingDate: Date(), endingDate: Date().addingTimeInterval(3600)), 
+             TimeSlot(startingDate: Date(), endingDate: Date().addingTimeInterval(3600))
+            ]), isEditable: false)
+    }
+}
