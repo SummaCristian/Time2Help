9fc3eddde8c096023ca4e96870d14cfa6108a53c
mattiaDina <mattia.dinardo@mail.polimi.it>
2024-12-27 22:29:33 +0100

    Update MapNeighbourhoodSelectorView.swift
    Inserted isSatelliteMode to save in mobile memory if the map have to display in satellite mode or in standard mode
    
    Modified variables needed for the map
    
    Modified paddings
    
    Inserted MapButtonsView
    
    Modified list of the neighbourhoods
    
    Modified animation for switch between map and list

--- Views/MapNeighbourhoodSelectorView.swift
+++ Views/MapNeighbourhoodSelectorView.swift
@@ -6,6 +6,8 @@ struct NeighbourhoodSelector: View {
     @Environment(\.dismiss) var dismiss
     @Environment(\.colorScheme) var colorScheme
     
+    @Binding var isSatelliteMode: Bool
+    
     @ObservedObject var viewModel: MapViewModel
     
     // The location selected by the User, nil if no selection has been made yet
@@ -14,7 +16,11 @@ struct NeighbourhoodSelector: View {
     
     @State private var neighbourhoodsUpdated: [Neighbourhood] = Database.neighbourhoods
     
+    // A NameSpace needed for certain Map features
+    @Namespace private var mapNameSpace
+    
     @State private var camera: MapCameraPosition = MapCameraPosition.automatic
+    @State private var cameraSupport: MapCameraPosition = MapCameraPosition.automatic
     
     @State private var showList: Bool = false
     
@@ -24,7 +30,8 @@ struct NeighbourhoodSelector: View {
             Map(
                 position: $camera,
                 bounds: MapCameraBounds(minimumDistance: 500, maximumDistance: .infinity),
-                interactionModes: .all
+                interactionModes: .all,
+                scope: mapNameSpace
             ) {
                 if viewModel.locationManager.authorizationStatus == .authorizedAlways || viewModel.locationManager.authorizationStatus == .authorizedWhenInUse {
                     // The User's Location
@@ -35,7 +42,7 @@ struct NeighbourhoodSelector: View {
                 // Note: it's set in selectedLocation if possible, but if it's nil, it defaults to the Favor's old Location
                 ForEach(neighbourhoodsUpdated) { neighbourhood in
                     Annotation("", coordinate: neighbourhood.location) {
-                        NeighbourhoodMarker(isSelected: .constant(neighbourhood.id == selectedNeighbourhoodStructTempTwo.id), neighbourhood: neighbourhood)
+                        NeighbourhoodMarker(isSelected: .constant(neighbourhood.id == selectedNeighbourhoodStructTempTwo.id), neighbourhood: neighbourhood, isSatelliteMode: isSatelliteMode)
                             .onTapGesture {
                                 selectedNeighbourhoodStructTempTwo = neighbourhood
                                 withAnimation(.spring(duration: 0.2)) {
@@ -47,22 +54,13 @@ struct NeighbourhoodSelector: View {
             }
             .mapControlVisibility(.visible)
             .mapControls {
-                if viewModel.locationManager.authorizationStatus == .authorizedAlways || viewModel.locationManager.authorizationStatus == .authorizedWhenInUse {
-                    MapUserLocationButton()
-                }
-                MapCompass()
                 MapScaleView()
-                MapPitchToggle()
             }
             .mapStyle(
-                .standard(
-                    elevation: .realistic,
-                    emphasis: .automatic,
-                    pointsOfInterest: .excludingAll
-                )
+                isSatelliteMode ? .hybrid(elevation: .realistic, pointsOfInterest: .all) : .standard(elevation: .realistic, emphasis: .automatic, pointsOfInterest: .all, showsTraffic: false)
             )
             .onAppear {
-                camera = MapCameraPosition.camera(.init(centerCoordinate: selectedNeighbourhoodStructTemp.location, distance: 3200))
+                camera = MapCameraPosition.camera(.init(centerCoordinate: selectedNeighbourhoodStructTempTwo.location, distance: 3200))
             }
             .onMapCameraChange(frequency: .onEnd) { camera in
                 let centerCoordinate = camera.camera.centerCoordinate
@@ -84,83 +82,79 @@ struct NeighbourhoodSelector: View {
                         neighbourhoodsUpdated[j].big = false
                     }
                 }
-                self.camera = MapCameraPosition.camera(.init(centerCoordinate: .init(latitude: centerCoordinate.latitude, longitude: centerCoordinate.longitude), distance: camera.camera.distance, heading: camera.camera.heading, pitch: camera.camera.pitch))
             }
-            .safeAreaPadding(.top, 110)
+            .safeAreaPadding(.top, 115)
             .safeAreaPadding(.leading, 5)
             .safeAreaPadding(.trailing, 10)
             .safeAreaPadding(.bottom, 80)
+            .overlay {
+                VStack(spacing: 0) {
+                    HStack(spacing: 0) {
+                        Spacer()
+                        
+                        MapButtonsView(viewModel: viewModel, camera: $camera, cameraSupport: $camera, isSatelliteMode: $isSatelliteMode, selectedCLLocationCoordinate: selectedNeighbourhoodStructTempTwo.location, mapNameSpace: mapNameSpace)
+                    }
+                        
+                    Spacer()
+                }
+                .padding(.top, 115)
+                .padding(.trailing, 10)
+            }
+            .onMapCameraChange(frequency: .continuous) { camera in
+                self.cameraSupport = MapCameraPosition.camera(.init(centerCoordinate: camera.camera.centerCoordinate, distance: camera.camera.distance, heading: camera.camera.heading, pitch: camera.camera.pitch))
+            }
+            .mapScope(mapNameSpace)
+            .animation(.easeInOut, value: camera)
             
             if showList {
-                Rectangle()
-                    .foregroundStyle(.thinMaterial)
-                    .safeAreaPadding(.top, 97)
-                    .edgesIgnoringSafeArea(.bottom)
-                
-                ScrollView {
-                    VStack(spacing: 20) {
-                        ForEach(Database.neighbourhoods) { neighbourhood in
-                            Button {
-                                selectedNeighbourhoodStructTempTwo = neighbourhood
-                                withAnimation(.spring(duration: 0.2)) {
-                                    camera = MapCameraPosition.camera(.init(centerCoordinate: .init(latitude: neighbourhood.location.latitude, longitude: neighbourhood.location.longitude), distance: camera.camera?.distance ?? 7200))
-                                }
-                                showList = false
-                            } label: {
-                                HStack {
-                                    Text(neighbourhood.name)
-                                        .foregroundStyle(colorScheme == .dark ? .white : .black)
-                                    
-                                    Spacer()
-                                }
-                                .overlay(alignment: .trailing) {
-                                    if neighbourhood.id == selectedNeighbourhoodStructTempTwo.id {
-                                        Image(systemName: "checkmark.circle.fill")
-                                            .font(.title2.bold())
-                                            .foregroundStyle(.green)
-                                    }
-                                }
-                                .padding(.all, 20)
-                                .background(colorScheme == .dark ? Color(.systemGray6) : .white, in: .rect(cornerRadius: 16))
+                List {
+                    ForEach(Database.neighbourhoods) { neighbourhood in
+                        Button {
+                            selectedNeighbourhoodStructTempTwo = neighbourhood
+                            withAnimation(.spring(duration: 0.2)) {
+                                camera = MapCameraPosition.camera(.init(centerCoordinate: .init(latitude: neighbourhood.location.latitude, longitude: neighbourhood.location.longitude), distance: camera.camera?.distance ?? 7200))
                             }
+                            showList = false
+                        } label: {
+                            HStack(spacing: 8) {
+                                Text(neighbourhood.name)
+                                    .foregroundStyle(colorScheme == .dark ? .white : .black)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                
+                                if neighbourhood.id == selectedNeighbourhoodStructTempTwo.id {
+                                    Image(systemName: "checkmark.circle.fill")
+                                        .font(.title2.bold())
+                                        .foregroundStyle(.green)
+                                }
+                            }
+                            .padding(.all, 8)
                         }
                     }
-                    .padding(.all, 20)
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(colorScheme == .dark ? .black : Color(.systemGray6), in: .rect)
-                .safeAreaPadding(.top, 97)
-                .padding(.bottom, 90)
+                .safeAreaPadding(.top, 105)
+                .safeAreaPadding(.bottom, 100)
                 
-//                VStack {
-//                    Spacer()
-//
-//
-//                        .frame(height: 90)
-//                        .ignoresSafeArea()
-//                }
+                Rectangle()
+                    .foregroundStyle(.thinMaterial)
+                    .frame(height: 130)
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+                    .edgesIgnoringSafeArea(.bottom)
             }
             
             // The Title bar
             VStack {
-//                VStack {
-//                    // Some space at the top
-//                    Spacer()
-//                        .frame(height: 10)
+                // The Text
+                HStack(spacing: 8) {
+                    Text("Clicca sul marcatore per selezionare il tuo quartiere")
+                        .font(.headline)
                     
-                    // The Text
-                    HStack(spacing: 8) {
-                        Text("Clicca sul marcatore per selezionare il tuo quartiere")
-                            .font(.headline)
-                        
-                        Spacer()
-                        
-                        NeighbourhoodMarker(isSelected: .constant(false), neighbourhood: .init(name: "", location: .init(), big: true))
-                            .padding(.bottom, -25)
-                    }
-                    .padding(.vertical, 25)
-                    .padding(.horizontal, 20)
-//                }
+                    Spacer()
+                    
+                    NeighbourhoodMarker(isSelected: .constant(false), neighbourhood: .init(name: "", location: .init(), big: true), isSatelliteMode: false)
+                        .padding(.bottom, -25)
+                }
+                .padding(.vertical, 25)
+                .padding(.horizontal, 20)
                 .background(.ultraThinMaterial)
                 
                 Spacer()
@@ -174,10 +168,10 @@ struct NeighbourhoodSelector: View {
                     Button {
                         showList.toggle()
                     } label: {
-                        Image(systemName: showList ? "map.fill" : "magnifyingglass")
+                        Image(systemName: showList ? "map.fill" : "list.dash")
                             .font(.title3.bold())
                             .foregroundStyle(.white)
-                            .padding(.all, 12)
+                            .frame(width: 50, height: 50)
                     }
                     .background(
                         Circle()
@@ -188,8 +182,8 @@ struct NeighbourhoodSelector: View {
                 Spacer()
             }
             .safeAreaPadding(.top, 115)
-            .safeAreaPadding(.trailing, showList ? 20 : 70)
-            .animation(.spring(duration: 0.4), value: showList)
+            .safeAreaPadding(.trailing, showList ? 10 : 70)
+            .animation(.easeInOut(duration: 0.4), value: showList)
             
             // The bottom Buttons
             VStack {
@@ -230,8 +224,8 @@ struct NeighbourhoodSelector: View {
                             .background(selectedNeighbourhoodStructTempTwo.id == selectedNeighbourhoodStructTemp.id ? Color(.systemGray5) : .green, in: .capsule)
                     }
                     .shadow(radius: 10)
-                    .hoverEffect(.highlight)
                     .disabled(selectedNeighbourhoodStructTempTwo.id == selectedNeighbourhoodStructTemp.id)
+                    .hoverEffect(.highlight)
                 }
             }
             .padding(.all, 20)
