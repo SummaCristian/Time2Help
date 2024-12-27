43d4ad36a29306efde4c398980d51467c9cfa7e8
Cristian Summa <cristian.summa@mail.polimi.it>
2024-12-27 12:03:26 +0100

    Updated CategoryFiltersView
    Moved the 'AdvancedFilters' Button outside of the ScrollView, so it doesn't move when scrolling through the chips

--- Views/CategoryFiltersView.swift
+++ Views/CategoryFiltersView.swift
@@ -16,55 +16,56 @@ struct CategoryFiltersView: View {
     
     var body: some View {
         GeometryReader { geometry in
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(alignment: isAdvancedFiltersViewShowing ? .top : .center, spacing: 10) {
-                    // Advanced Filters Button
-                    ZStack {
-                        if !isAdvancedFiltersViewShowing {
-                            Menu {
-                                Button ("Filtri avanzati", systemImage: "line.3.horizontal.decrease") {
-                                    openAdvancedFilters()
-                                }
-                                
-                                
-                                Button("Reset", systemImage: "arrow.counterclockwise", role: .destructive) {
-                                    filter.reset()
-                                }
-                                
-                            } label: {
-                                Image(systemName: "line.3.horizontal.decrease")
-                                    .font(.system(size: 20, weight: .medium))
-                                    .padding(10)
-                                    .foregroundStyle(.primary)
-                            }
-                            primaryAction: {
+            
+            HStack(alignment: isAdvancedFiltersViewShowing ? .top : .center, spacing: 10) {
+                // Advanced Filters Button
+                ZStack {
+                    if !isAdvancedFiltersViewShowing {
+                        Menu {
+                            Button ("Filtri avanzati", systemImage: "line.3.horizontal.decrease") {
                                 openAdvancedFilters()
                             }
-                            .scaleEffect(moveToCenter ? 1.5 : 1)
-                            .clipShape(Circle())
-                            .hoverEffect(.lift)
-                            .matchedGeometryEffect(id: "advancedFilters", in: animationNamespace)
-                        } else {
-                            AdvancedFiltersView(
-                                screenWidth: geometry.size.width,
-                                screenHeight: geometry.size.height,
-                                filter: filter,
-                                isAdvancedFiltersViewShowing: $startClosing
-                            )
-                            .matchedGeometryEffect(id: "advancedFilters", in: animationNamespace)
-                            .clipShape(RoundedRectangle(cornerRadius: 32))
-                            .blur(radius: blurContainer ? 10 : 0)
-                            .padding(5)
+                            
+                            
+                            Button("Reset", systemImage: "arrow.counterclockwise", role: .destructive) {
+                                filter.reset()
+                            }
+                            
+                        } label: {
+                            Image(systemName: "line.3.horizontal.decrease")
+                                .font(.system(size: 20, weight: .medium))
+                                .padding(10)
+                                .foregroundStyle(.primary)
                         }
+                    primaryAction: {
+                        openAdvancedFilters()
                     }
-                    .frame(minHeight: 60)
-                    .background(.thinMaterial)
-                    .clipShape(isAdvancedFiltersViewShowing ? AnyShape(RoundedRectangle(cornerRadius: 32)) : AnyShape(Circle()))
-                    .offset(
-                        x: moveToCenter ? 130 : 0, 
-                        y: moveToCenter ? 200 : 0)
-                    .shadow(radius: 3)
-                    
+                    .scaleEffect(moveToCenter ? 1.5 : 1)
+                    .clipShape(Circle())
+                    .hoverEffect(.lift)
+                    .matchedGeometryEffect(id: "advancedFilters", in: animationNamespace)
+                    } else {
+                        AdvancedFiltersView(
+                            screenWidth: geometry.size.width,
+                            screenHeight: geometry.size.height,
+                            filter: filter,
+                            isAdvancedFiltersViewShowing: $startClosing
+                        )
+                        .matchedGeometryEffect(id: "advancedFilters", in: animationNamespace)
+                        .clipShape(RoundedRectangle(cornerRadius: 32))
+                        .blur(radius: blurContainer ? 10 : 0)
+                        .padding(5)
+                    }
+                }
+                .frame(minHeight: 60)
+                .background(.thinMaterial)
+                .clipShape(isAdvancedFiltersViewShowing ? AnyShape(RoundedRectangle(cornerRadius: 32)) : AnyShape(Circle()))
+                .offset(
+                    x: moveToCenter ? 130 : 0, 
+                    y: moveToCenter ? 200 : 0)
+                .shadow(radius: 3)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 10) {
                         // Real Categories
                         ForEach(FavorCategory.allCases) { category in
@@ -73,13 +74,19 @@ struct CategoryFiltersView: View {
                                     .onTapGesture {
                                         if category == .all {
                                             // The "Tutte" capsule
-                                            filter.selectCategory(category: .all)
+                                            withAnimation {
+                                                filter.selectCategory(category: .all)
+                                            }
                                         } else {
                                             // The real categories
                                             if filter.selectedCategories.contains(category) {
-                                                filter.deselectCategory(category: category)
+                                                withAnimation {
+                                                    filter.deselectCategory(category: category)
+                                                }
                                             } else {
-                                                filter.selectCategory(category: category)
+                                                withAnimation {
+                                                    filter.selectCategory(category: category)
+                                                }
                                             }
                                         }
                                     }
@@ -88,9 +95,9 @@ struct CategoryFiltersView: View {
                     }
                     .padding(.vertical, 8)
                 }
-                .padding(.horizontal)
-                .frame(alignment: .top)
             }
+            .padding(.horizontal)
+            .frame(alignment: .top)
             .scrollDisabled(isAdvancedFiltersViewShowing)
         }
         .sensoryFeedback(.impact, trigger: moveToCenter, condition: { old, new in
