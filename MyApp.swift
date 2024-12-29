46864591d4c539e91e9d852d306b51a15d04d33d
mattiaDina <mattia.dinardo@mail.polimi.it>
2024-12-29 17:37:45 +0100

    Update MyApp.swift
    Removed isSatelliteMode because don't needed in the view

--- MyApp.swift
+++ MyApp.swift
@@ -7,16 +7,15 @@ struct AppTime2Help2App: App {
     @AppStorage("nameSurname") var nameSurname: String = ""
     @AppStorage("selectedColor") var selectedColor: String = "blue"
     @AppStorage("selectedNeighbourhood") var selectedNeighbourhood: String = "Duomo"
-    @AppStorage("mapStyle") private var isSatelliteMode: Bool = false
     
     @StateObject private var viewModel = MapViewModel()
     
     var body: some Scene {
         WindowGroup {
             if showLogin {
-                LoginView(showLogin: $showLogin, isSatelliteMode: $isSatelliteMode, viewModel: viewModel, nameSurname: $nameSurname, selectedColor: $selectedColor, selectedNeighbourhood: $selectedNeighbourhood)
+                LoginView(showLogin: $showLogin, viewModel: viewModel, nameSurname: $nameSurname, selectedColor: $selectedColor, selectedNeighbourhood: $selectedNeighbourhood)
             } else {
-                ContentView(isSatelliteMode: $isSatelliteMode, viewModel: viewModel, nameSurname: $nameSurname, selectedColor: $selectedColor, selectedNeighbourhood: $selectedNeighbourhood)
+                ContentView(viewModel: viewModel, nameSurname: $nameSurname, selectedColor: $selectedColor, selectedNeighbourhood: $selectedNeighbourhood)
             }
         }
     }
