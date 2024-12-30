72695e17300811f39ea3e2618e53d862368860fd
Cristian Summa <cristian.summa@mail.polimi.it>
2024-12-30 16:41:55 +0100

    Added FaqScreen
    Added a screen to display the FAQs

---
+++ Views/FAQ/FaqScreen.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct faqScreen: View {
+    var body: some View {
+        Form {
+            HStack {
+                Spacer()
+                
+                Image(systemName: "questionmark.bubble.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .foregroundStyle(.secondary)
+                    .shadow(radius: 10)
+                
+                Spacer()
+            }
+                .listRowBackground(Color.clear)
+            
+            Section(
+                content: {
+                    List {
+                        ForEach(FAQs.allCases) { faq in
+                            faqView(faq: faq.faq)
+                        }
+                    }
+                }, header: {
+                    Text("Domande chieste frequentemente (FAQ)")
+                }
+            )
+        }
+        .navigationTitle("Domande")
+    }
+}
+
+#Preview {
+    faqScreen()
+}
