fe7f94e7620a03d8c46b2e73992ee7f1f906dd43
mattiaDina <mattia.dinardo@mail.polimi.it>
2024-12-30 12:13:12 +0100

    Update MapNeighbourhoodSelectorView.swift
    The view has been completely changed with the possibility of switching between a map and a list of neighborhoods. The list has a custom scroll view that scrolls directly to the selected neighborhood when it appears, a custom search bar and screens that indicate when you are trying to enter something to start searching and if the neighborhood has not been found

--- Views/LoginScreens/MapNeighbourhoodSelectorView.swift
+++ Views/LoginScreens/MapNeighbourhoodSelectorView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 import MapKit
 
 struct NeighbourhoodSelector: View {
+    
     // Used to control the dismissal from inside the sheet
     @Environment(\.dismiss) var dismiss
     @Environment(\.colorScheme) var colorScheme
@@ -13,6 +14,7 @@ struct NeighbourhoodSelector: View {
     @Binding var selectedNeighbourhoodStructTempTwo: Neighbourhood
     
     @AppStorage("mapStyle") private var isSatelliteMode: Bool = false
+    @AppStorage("showListNeighbourhood") private var showListNeighbourhood: Bool = false
     
     @State private var neighbourhoodsUpdated: [Neighbourhood] = Database.neighbourhoods
     
@@ -22,7 +24,19 @@ struct NeighbourhoodSelector: View {
     @State private var camera: MapCameraPosition = MapCameraPosition.automatic
     @State private var cameraSupport: MapCameraPosition = MapCameraPosition.automatic
     
-    @State private var showList: Bool = false
+    @State private var searchText: String = ""
+    @FocusState private var searchFocused: Bool
+    @State private var showCancel: Bool = false
+    
+    var searchResult: [Neighbourhood] {
+        if !searchFocused && searchText.isEmpty {
+            return Database.neighbourhoods
+        } else if searchText.isEmpty {
+            return []
+        } else {
+            return Database.neighbourhoods.filter({ $0.name.lowercased().contains(searchText.lowercased()) })
+        }
+    }
     
     // The UI
     var body: some View {
@@ -93,13 +107,13 @@ struct NeighbourhoodSelector: View {
                     HStack(spacing: 0) {
                         Spacer()
                         
-                        MapButtonsView(viewModel: viewModel, camera: $camera, cameraSupport: $cameraSupport, selectedCLLocationCoordinate: selectedNeighbourhoodStructTempTwo.location, mapNameSpace: mapNameSpace)
+                        MapButtonsView(viewModel: viewModel, camera: $camera, cameraSupport: $camera, selectedCLLocationCoordinate: selectedNeighbourhoodStructTempTwo.location, mapNameSpace: mapNameSpace)
                     }
                     
                     Spacer()
                 }
-                .padding(.top, 115)
-                .padding(.trailing, 10)
+                .padding(.top, 170)
+                .padding(.trailing, 15)
             }
             .onMapCameraChange(frequency: .continuous) { camera in
                 self.cameraSupport = MapCameraPosition.camera(.init(centerCoordinate: camera.camera.centerCoordinate, distance: camera.camera.distance, heading: camera.camera.heading, pitch: camera.camera.pitch))
@@ -107,85 +121,174 @@ struct NeighbourhoodSelector: View {
             .mapScope(mapNameSpace)
             .animation(.easeInOut, value: camera)
             
-            if showList {
-                List {
-                    ForEach(Database.neighbourhoods) { neighbourhood in
-                        Button {
-                            selectedNeighbourhoodStructTempTwo = neighbourhood
-                            withAnimation(.spring(duration: 0.2)) {
-                                camera = MapCameraPosition.camera(.init(centerCoordinate: .init(latitude: neighbourhood.location.latitude, longitude: neighbourhood.location.longitude), distance: camera.camera?.distance ?? 7200))
-                            }
-                            showList = false
-                        } label: {
-                            HStack(spacing: 8) {
-                                Text(neighbourhood.name)
-                                    .foregroundStyle(colorScheme == .dark ? .white : .black)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                
-                                if neighbourhood.id == selectedNeighbourhoodStructTempTwo.id {
-                                    Image(systemName: "checkmark.circle.fill")
-                                        .font(.title2.bold())
-                                        .foregroundStyle(.green)
+            if showListNeighbourhood {
+                ZStack {
+                    if (searchText.isEmpty && searchFocused) || (!searchText.isEmpty && searchResult.isEmpty) {
+                        VStack(spacing: 20) {
+                            Image(systemName: searchText.isEmpty && searchFocused ? "keyboard" : "eyes")
+                                .font(.system(size: 80))
+                                .overlay(alignment: .topTrailing) {
+                                    if !searchText.isEmpty && searchResult.isEmpty {
+                                        Image(systemName: "questionmark")
+                                            .font(.system(size: 30))
+                                            .padding(.top, -10)
+                                            .padding(.trailing, -16)
+                                    }
+                                }
+                            
+                            Text(searchText.isEmpty && searchFocused ? "Scrivi una lettera per iniziare a cercare" : "Nessun quartiere trovato")
+                                .font(.title2)
+                                .multilineTextAlignment(.center)
+                        }
+                        .foregroundStyle(.gray)
+                        .bold()
+                        .padding(.all, 60)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                    } else {
+                        ScrollViewReader { reader in
+                            ScrollView {
+                                VStack(spacing: 16) {
+                                    ForEach(searchResult) { neighbourhood in
+                                        Button {
+                                            selectedNeighbourhoodStructTempTwo = neighbourhood
+                                            withAnimation(.spring(duration: 0.2)) {
+                                                camera = MapCameraPosition.camera(.init(centerCoordinate: .init(latitude: neighbourhood.location.latitude, longitude: neighbourhood.location.longitude), distance: camera.camera?.distance ?? 7200))
+                                            }
+                                        } label: {
+                                            HStack(spacing: 8) {
+                                                Text(neighbourhood.name)
+                                                    .foregroundStyle(colorScheme == .dark ? .white : .black)
+                                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                                
+                                                NeighbourhoodMarker(isSelected: .constant(true), neighbourhood: .init(name: "", location: .init(), big: false))
+                                                    .scaleEffect(0.6)
+                                                    .padding(.bottom, -15)
+                                                    .opacity(neighbourhood.id == selectedNeighbourhoodStructTempTwo.id ? 1 : 0)
+                                            }
+                                            .padding(.vertical, -8)
+                                            .padding(.leading, 8)
+                                        }
+                                        .padding(.all, 16)
+                                        .background(colorScheme == .dark ? Color(.systemGray6) : .white, in: .rect(cornerRadius: 25))
+                                        .id(neighbourhood.id)
+                                    }
+                                }
+                                .padding(.all, 20)
+                                .onAppear {
+                                    reader.scrollTo(selectedNeighbourhoodStructTempTwo.id, anchor: .top)
                                 }
                             }
-                            .padding(.all, 8)
+                            .scrollDismissesKeyboard(.immediately)
                         }
                     }
                 }
-                .safeAreaPadding(.top, 105)
+                .safeAreaPadding(.top, searchFocused ? 70 : 170)
                 .safeAreaPadding(.bottom, 100)
-                
-                Rectangle()
-                    .foregroundStyle(.thinMaterial)
-                    .frame(height: 130)
-                    .frame(maxHeight: .infinity, alignment: .bottom)
-                    .edgesIgnoringSafeArea(.bottom)
+                .background(.regularMaterial)
             }
             
             // The Title bar
             VStack {
-                // The Text
-                HStack(spacing: 8) {
-                    Text("Clicca sul marcatore per selezionare il tuo quartiere")
-                        .font(.headline)
+                VStack(spacing: 0) {
+                    if !searchFocused {
+                        // The Text
+                        HStack(spacing: 8) {
+                            Text("Clicca " + (showListNeighbourhood ? "il nome" : "sul marcatore") + " per selezionare il tuo quartiere")
+                                .font(.headline)
+                            
+                            Spacer()
+                            
+                            NeighbourhoodMarker(isSelected: .constant(false), neighbourhood: .init(name: "", location: .init(), big: true))
+                                .padding(.bottom, -25)
+                        }
+                        .padding(.vertical, 25)
+                        .padding(.horizontal, 20)
+                        .frame(height: !searchFocused ? 100 : 0)
+                        .opacity(!searchFocused ? 1 : 0)
+                    }
                     
-                    Spacer()
-                    
-                    NeighbourhoodMarker(isSelected: .constant(false), neighbourhood: .init(name: "", location: .init(), big: true))
-                        .padding(.bottom, -25)
+                    HStack(spacing: 12) {
+                        if showListNeighbourhood {
+                            ZStack(alignment: .trailing) {
+                                Button {
+                                    searchText = ""
+                                    
+                                    showCancel = false
+                                    
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                        searchFocused = false
+                                    }
+                                } label: {
+                                    Text("Annulla")
+                                }
+                                .opacity(showCancel ? 1 : 0)
+                                .scaleEffect(showCancel ? 1 : 0)
+                                
+                                HStack(spacing: 8) {
+                                    TextField(text: $searchText) {
+                                        Text("Cerca")
+                                    }
+                                    .focused($searchFocused)
+                                    
+                                    if !searchText.isEmpty {
+                                        Image(systemName: "xmark.circle.fill")
+                                            .font(.title3)
+                                            .foregroundStyle(.red, .red.opacity(0.2))
+                                            .onTapGesture {
+                                                searchText = ""
+                                            }
+                                    }
+                                }
+                                .frame(height: 45)
+                                .padding(.leading, 16)
+                                .padding(.trailing, 12)
+                                .background(colorScheme == .dark ? Color(.systemGray6) : .white, in: .rect(cornerRadius: 12))
+                                .padding(.trailing, searchFocused ? 70 : 0)
+                            }
+                            .padding(.trailing, 4)
+                        } else {
+                            Spacer()
+                        }
+                        
+                        Button {
+                            showListNeighbourhood.toggle()
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                                if !searchText.isEmpty {
+                                    searchText = ""
+                                }
+                            }
+                            
+                            if searchFocused {
+                                searchFocused = false
+                            }
+                            
+                            if showCancel {
+                                showCancel = false
+                            }
+                        } label: {
+                            Image(systemName: showListNeighbourhood ? "map.fill" : "list.bullet")
+                                .font(.title3.bold())
+                                .foregroundStyle(.white)
+                                .frame(width: 45, height: 45)
+                        }
+                        .background(
+                            Circle()
+                                .foregroundStyle(.blue)
+                        )
+                    }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 17.5)
                 }
-                .padding(.vertical, 25)
-                .padding(.horizontal, 20)
+                .padding(.bottom, showListNeighbourhood ? 0 : -70)
                 .background(.ultraThinMaterial)
                 
                 Spacer()
             }
-            
-            //            Text("\(altitude)")
-            VStack(alignment: .leading) {
-                HStack {
-                    Spacer()
-                    
-                    Button {
-                        showList.toggle()
-                    } label: {
-                        Image(systemName: showList ? "map.fill" : "list.dash")
-                            .font(.title3.bold())
-                            .foregroundStyle(.white)
-                            .frame(width: 50, height: 50)
-                    }
-                    .background(
-                        Circle()
-                            .foregroundStyle(.blue)
-                    )
-                }
-                
-                Spacer()
-            }
-            .safeAreaPadding(.top, 115)
-            .safeAreaPadding(.trailing, showList ? 10 : 70)
-            .animation(.easeInOut(duration: 0.4), value: showList)
-            
+        }
+        .presentationDragIndicator(.hidden)
+        .ignoresSafeArea(.keyboard)
+        .overlay {
             // The bottom Buttons
             VStack {
                 Spacer()
@@ -203,7 +306,7 @@ struct NeighbourhoodSelector: View {
                         Label("Annulla", systemImage: "xmark")
                             .font(.body.bold())
                             .foregroundStyle(.white)
-                            .padding(.vertical, 15)
+                            .padding(.vertical, searchFocused ? 12 : 15)
                             .frame(maxWidth: .infinity)
                             .background(.red, in: .capsule)
                     }
@@ -220,7 +323,7 @@ struct NeighbourhoodSelector: View {
                         Label("Seleziona", systemImage: "checkmark")
                             .font(.body.bold())
                             .foregroundStyle(selectedNeighbourhoodStructTempTwo.id == selectedNeighbourhoodStructTemp.id ? .gray : .white)
-                            .padding(.vertical, 15)
+                            .padding(.vertical, searchFocused ? 12 : 15)
                             .frame(maxWidth: .infinity)
                             .background(selectedNeighbourhoodStructTempTwo.id == selectedNeighbourhoodStructTemp.id ? Color(.systemGray5) : .green, in: .capsule)
                     }
@@ -228,10 +331,21 @@ struct NeighbourhoodSelector: View {
                     .disabled(selectedNeighbourhoodStructTempTwo.id == selectedNeighbourhoodStructTemp.id)
                     .hoverEffect(.highlight)
                 }
+                .padding([.top, .horizontal], 20)
+                .padding(.bottom, searchFocused ? 8 : 20)
+                .background(.ultraThinMaterial.opacity(showListNeighbourhood ? 0 : 1))
             }
-            .padding(.all, 20)
         }
-        .presentationDragIndicator(.hidden)
+        .animation(.bouncy(duration: 0.4), value: searchFocused)
+        .animation(.easeInOut(duration: 0.4), value: showListNeighbourhood)
+        .animation(.bouncy(duration: 0.4), value: showCancel)
+        .onChange(of: searchFocused) { _, new in
+            if new {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    showCancel = true
+                }
+            }
+        }
     }
 }
 
