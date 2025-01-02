9509a3998a37a7ddce92ea42f177b4e2909ab63c
mattiaDina <mattia.dinardo@mail.polimi.it>
2025-01-03 00:31:27 +0100

    Update CategoryPickerView.swift
    Modified the icon for all categories selected
    
    Modified the if for selected categories to show the exact text in each status of selected categories

--- Views/MapTab/CategoryPickerView.swift
+++ Views/MapTab/CategoryPickerView.swift
@@ -10,14 +10,31 @@ struct CategoryPicker: View {
     var body: some View {
         VStack(spacing: 16) {
             Toggle(isOn: $isAllCategorySelected, label: {
-                HStack(spacing: 8) {
-                    Image(systemName: FavorCategory.all.icon)
-                        .foregroundStyle(Color(.white))
-                        .frame(width: 30, height: 30)
-                        .background {
-                            RoundedRectangle(cornerRadius: 5)
-                                .foregroundStyle(Color(.systemYellow))
+                HStack(spacing: 12) {
+                    VStack(spacing: 0) {
+                        HStack(spacing: 0) {
+                            Image(systemName: FavorCategory.generic.icon)
+                                .frame(width: 12, height: 12)
+                            Image(systemName: FavorCategory.gardening.icon)
+                                .frame(width: 12, height: 12)
                         }
+                        HStack(spacing: 0) {
+                            Image(systemName: FavorCategory.transport.icon)
+                                .frame(width: 12, height: 12)
+                            Image(systemName: "magnifyingglass")
+                                .fontWeight(.bold)
+                                .frame(width: 12, height: 12)
+                                .offset(x: 1)
+                        }
+                    }
+                    .font(.system(size: 8))
+                    .foregroundStyle(Color(.white))
+                    .padding(.all, 6)
+                    .frame(width: 30, height: 30)
+                    .background {
+                        RoundedRectangle(cornerRadius: 5)
+                            .foregroundStyle(Color(.systemYellow))
+                    }
                     
                     VStack(alignment: .leading) {
                         Text("Tutte")
@@ -32,23 +49,25 @@ struct CategoryPicker: View {
             
             if showCategories {
                 VStack {
-                    if count != 0 && !filterModel.isCategorySelected(category: .all) {
-                        HStack(spacing: 0) {
-                            Text(String(count))
-                                .font(.subheadline.bold())
-                                .contentTransition(.numericText())
-                            
-                            Text(" categorie selezionate")
-                                .font(.subheadline.bold())
-                        }
-                    } else if filterModel.isCategorySelected(category: .all){
+                    if filterModel.selectedCategories.contains(.all) {
                         Text("Tutte le categorie selezionate")
                             .font(.subheadline.bold())
                             .foregroundStyle(.blue)
                     } else {
-                        Text("Nessuna categoria selezionata")
-                            .font(.subheadline.bold())
-                            .foregroundStyle(.red)
+                        if filterModel.selectedCategories.count == 0 {
+                            Text("Nessuna categoria selezionata")
+                                .font(.subheadline.bold())
+                                .foregroundStyle(.red)
+                        } else {
+                            HStack(spacing: 0) {
+                                Text(String(count))
+                                    .font(.subheadline.bold())
+                                    .contentTransition(.numericText())
+                                
+                                Text(" categorie selezionate")
+                                    .font(.subheadline.bold())
+                            }
+                        }
                     }
                     
                     LazyVGrid(columns: Array(repeating: .init(.fixed(55)), count: 4), spacing: 0) {
