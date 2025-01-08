6e5933a274b48c4fc0e654c7deb94d436a0baee3
mattiaDina <mattia.dinardo@mail.polimi.it>
2025-01-08 23:31:10 +0100

    Update FaqScreen.swift
    Added the variable that makes the explanation view appear, the related button and the variable to deactivate the tour button when you are in the explanation screen

--- Views/FAQ/FaqScreen.swift
+++ Views/FAQ/FaqScreen.swift
@@ -1,6 +1,10 @@
 import SwiftUI
 
 struct FaqScreen: View {
+    
+    let isInExplanationView: Bool
+    @Binding var showExplanationTemp: Bool
+    
     var body: some View {
         Form {
             HStack {
@@ -30,9 +34,39 @@ struct FaqScreen: View {
             )
         }
         .navigationTitle("Domande")
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                // Guided tour
+                Button {
+                    withAnimation {
+                        showExplanationTemp = true
+                    }
+                } label: {
+                    HStack(spacing: 8) {
+                        Image(systemName: "figure.wave")
+                        
+                        Text("Tour")
+                    }
+                    .font(.subheadline)
+                    .padding(.vertical, 6)
+                    .padding(.horizontal, 16)
+                    .foregroundColor(.white)
+                    .background {
+                        if isInExplanationView {
+                            Capsule()
+                                .foregroundStyle(.gray.gradient)
+                        } else {
+                            Capsule()
+                                .foregroundStyle(.blue.gradient)
+                        }
+                    }
+                }
+                .disabled(isInExplanationView)
+            }
+        }
     }
 }
 
 #Preview {
-    FaqScreen()
+//    FaqScreen(showExplanationTemp: .constant(false))
 }
